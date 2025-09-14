926aab3e73db188fcd7af321e64e45695207f5b8
magalaReuben <magalareuben60@gmail.com>
2025-09-15 02:50:33 +0300

    postgresql

--- beepdata_backend/settings.py
+++ beepdata_backend/settings.py
@@ -69,8 +69,12 @@ WSGI_APPLICATION = 'beepdata_backend.wsgi.application'
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'dfKMcpJHTqRizvxAprgjbTeptKhSmNUe',
+        'HOST': 'crossover.proxy.rlwy.net',
+        'PORT': '39499',
     }
 }
 
